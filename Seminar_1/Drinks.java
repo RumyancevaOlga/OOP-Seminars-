fed360b83f05f59b9554210e8d02de9bc332f310
Rumyanceva <nj89.89@mail.ru>
2022-12-16 13:57:19 +0300

    Add Seminar1 and HomeWork1

---
+++ Seminar_1/Drinks.java
@@ -0,0 +1,8 @@
+package Seminar_1;
+
+public class Drinks extends VendingMachine{
+    @Override
+    public void restore() {
+        System.out.println("Напиток выдан");
+    }
+}
---
+++ Seminar_1/Item.java
@@ -0,0 +1,24 @@
+package Seminar_1;
+
+public class Item extends Product{
+    private double weight;
+
+    public Item (String name, double price, double weight) {
+        super(name, price);
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public void doSmth() {
+        System.out.println("Это класс элемент");
+        weight += 1;
+    }
+}
